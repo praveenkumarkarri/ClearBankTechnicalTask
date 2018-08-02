3cf4ea79884c6ca541bb0530f995749252e730f3
unknown <praveenkumarkarri17@gmail.com>
2018-08-02 16:54:56 +0100

    Added feature file and corresponding step definition file

---
+++ DeckCardProject/DeckCardProject/Features/DeckCard.feature
@@ -0,0 +1,9 @@
+﻿Feature: DeckCard
+The following Scenario checks for the number of cards remaining
+after certain cards are drawn from teh deck
+
+
+Scenario: Check if remaining cards count is correctly returned from the backend
+	Given I create the new deck
+	When I draw the cards 5 times from the deck 
+	Then the remaining cards count of the deck is accurate
---
+++ DeckCardProject/DeckCardProject/Steps/DeckCardSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace DeckCardProject.Steps
+{
+    [Binding]
+    class DeckCardSteps
+    {
+        [Test]
+        [Given(@"I create the new deck")]
+        public void GivenICreateTheNewDeck()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I draw the cards (.*) times from the deck")]
+        public void WhenIDrawTheCardsTimesFromTheDeck(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the remaining cards count of the deck is accurate")]
+        public void ThenTheRemainingCardsCountOfTheDeckIsAccurate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
