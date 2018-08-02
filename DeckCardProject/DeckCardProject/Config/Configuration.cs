ce64f6a6714adab4c418d24758209c07c098898a
unknown <praveenkumarkarri17@gmail.com>
2018-08-02 16:48:50 +0100

    Initial commit - storing static values in the Configuration file

---
+++ DeckCardProject/DeckCardProject/Config/Configuration.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Configuration;
+
+namespace DeckCardProject.Config
+{
+    static class Configuration
+    {
+        static Configuration()
+        {
+            BaseURL = ConfigurationManager.AppSettings["BaseURL"].ToString();
+            RelativeURL = ConfigurationManager.AppSettings["RelativeURL"].ToString();
+        }
+
+        public static string BaseURL { get; set; }
+        public static string RelativeURL { get; set; }
+    }
+}
